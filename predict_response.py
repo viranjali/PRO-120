c5a8fd7b9e2634039d1782696231447b7d3b3871
viranjali <76208580+viranjali@users.noreply.github.com>
2024-01-05 19:47:25 +0530

    Add files via upload

---
+++ predict_response.py
@@ -0,0 +1,55 @@
+import nltk
+
+import json
+import pickle
+import numpy as np
+import random
+
+ignore_words = ['?', '!',',','.', "'s", "'m"]
+
+# Model Load Lib
+import tensorflow
+from data_preprocessing import get_stem_words
+model=tensorflow.keras.models.load_model("./chatbot_model.h5")
+
+intents=json.loads(open('./intents.json').read())
+words=pickle.load(open('./words.pkl','rb'))
+classes=pickle.load(open('./classes.pkl','rb'))
+
+def preprocess_user_input(user_input):
+    input_word_token_1=nltk.word_tokenize(user_input) # [hello there!!!!!] ["hello","there","!","!"]
+    input_word_token_2=get_stem_words(input_word_token_1,ignore_words)
+    input_word_token_2=sorted(list(set(input_word_token_2))) #["hello","there"]
+
+    bag=[]
+    bag_of_words=[]
+
+    for word in words:
+        if word in input_word_token_2:
+            bag_of_words.append(1)
+        else:
+            bag_of_words.append(0)
+    bag.append(bag_of_words)
+    return np.array(bag)
+
+def bot_class_prediction(user_input):
+    inp=preprocess_user_input(user_input) # hello there
+    prediction=model.predict(inp)
+    predicted_class_label=np.argmax(prediction[0])
+    return predicted_class_label
+
+def bot_response(user_input):
+    predicted_class_label=bot_class_prediction(user_input)
+    predicted_class=classes[predicted_class_label] # greeting
+    for intent in intents['intents']:
+        if intent['tag']==predicted_class:
+            bot_response=random.choice(intent['responses'])
+            return bot_response
+print("Hi I am Stella, How Can I help you?")
+while True:
+
+    # take input from the user
+    user_input = input('Type you message here : ')
+
+    response = bot_response(user_input)
+    print("Bot Response: ", response)
